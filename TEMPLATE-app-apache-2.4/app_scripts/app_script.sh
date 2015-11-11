wooyea/apps

commit 769049526a35a1cbbe61dd0c6cdeaae8be66e1a2

[apache-2.4] Detect modules dir beforce start up.

TEMPLATE-app-apache-2.4/app_scripts/app_script.sh
@@ -58,6 +58,12 @@ ULIMIT_MAX_FILES="ulimit -S -n `ulimit -H -n`"
 # --------------------                              --------------------
 # ||||||||||||||||||||   END CONFIGURATION SECTION  ||||||||||||||||||||
 
+# check enviroment depented dirs
+[ -d "$APP_DIR/modules" ] || echo "ERROR!!! $APP_DIR/modules not found!" \
+  || "make a soft link name  $APP_DIR/modules linked to modules dir under apache install dir."
+  || exit 1;
+
+
 # Set the maximum number of file descriptors allowed per child process.
 if [ "x$ULIMIT_MAX_FILES" != "x" ] ; then
     $ULIMIT_MAX_FILES

TEMPLATE-app-apache-2.4/modules
@@ -1,1 +1,0 @@
-/usr/lib/httpd/modules/

commit 129320101fafb4df7be57a958161be85a62f43b7

[apache-2.4] Fix grammer error, introduced by 769049526a

TEMPLATE-app-apache-2.4/app_scripts/app_script.sh
@@ -60,7 +60,7 @@ ULIMIT_MAX_FILES="ulimit -S -n `ulimit -H -n`"
 
 # check enviroment depented dirs
 [ -d "$APP_DIR/modules" ] || echo "ERROR!!! $APP_DIR/modules not found!" \
-  || "make a soft link name  $APP_DIR/modules linked to modules dir under apache install dir."
+  || "make a soft link name  $APP_DIR/modules linked to modules dir under apache install dir." \
   || exit 1;
 
 