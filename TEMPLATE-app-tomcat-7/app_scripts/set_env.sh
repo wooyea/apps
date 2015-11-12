339a0eb1411fa2a02aa79bf4f8e94d13f418bde2
wooyea <wooyea@gmail.com>
2015-11-12 16:27:46 +0800

    [tomcat-7] chage default log path.

--- TEMPLATE-app-tomcat-7/app_scripts/set_env.sh
+++ TEMPLATE-app-tomcat-7/app_scripts/set_env.sh
@@ -17,7 +17,7 @@ export JAVA_HOME=/data1/common/jdk1.7
 export TOMCAT_HOME=/data1/common/apache-tomcat-7.0.65
 export CATALINA_BASE=$APP_DIR
 export CATALINA_PID=$APP_DIR/var/${APP_NAME}.pid
-export CATALINA_LOG=/log1/sunbuyer/${APP_NAME}
+export CATALINA_LOG=$APP_DIR/logs
 mkdir -p $CATALINA_LOG
 export CATALINA_OUT=$CATALINA_LOG/catalina.out
 export CATALINA_OPTS=" -DCATALINA_LOG=$CATALINA_LOG"
