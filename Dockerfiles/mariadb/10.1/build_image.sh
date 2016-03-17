a734fa7ae758847316cc355ff22a82d23e1f1db6
wooyea <wooyea@gmail.com>
2016-03-17 09:56:23 +0800

    Add dockers mariadb10.1 php5.6

---
+++ Dockerfiles/mariadb/10.1/build_image.sh
@@ -0,0 +1,5 @@
+#docker pull mariadb:10.1
+docker build -t tggj/mariadb:10.1 ./
+
+
+docker tag tggj/mariadb:10.1 registry.aliyuncs.com/wql/mariadb:10.1
---
+++ Dockerfiles/php/5.6/build_image.sh
@@ -0,0 +1,9 @@
+[ -f "php-5.6.18.tar.xz" ] || wget http://php.net/get/php-5.6.18.tar.xz/from/this/mirror
+
+ping mirrors.aliyuncs.com -c 2
+[ $? -eq 0 ] && cp ../../common/sources.list.jessie-aliyun sources.list.jessie \
+    || cp ../../common/sources.list.jessie-163 sources.list.jessie
+
+docker build -t tggj/php:5.6 ./
+
+rm sources.list.jessie
