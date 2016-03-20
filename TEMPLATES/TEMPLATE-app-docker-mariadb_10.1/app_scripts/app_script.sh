9e4cbc5ee41a9a4e08d2d58e3ce157389d99bedf
wooyea <wooyea@gmail.com>
2016-03-20 15:41:48 +0800

    [mariadb] fixbug cannot switch to run user by use new docker file

--- TEMPLATES/TEMPLATE-app-docker-mariadb_10.1/app_scripts/app_script.sh
+++ TEMPLATES/TEMPLATE-app-docker-mariadb_10.1/app_scripts/app_script.sh
@@ -21,39 +21,27 @@ export PRG_RELATIVE_DIR=`dirname "$PRG"`
 source $PRG_RELATIVE_DIR/../../common/header.sh
 ###### APPS header end #######################################################
 
-export ROOT_PASSWORD=w16eqwED 
-
 app_init() {
+    
+    [ -d $DATA_DIR ] || mkdir $DATA_DIR  || sudo chown $RUN_USER:$RUN_GROUP $DATA_DIR 
+
     docker create --net=host                    \
         --name $DOCKER_CONTAINER_NAME           \
+        -u $RUN_USER_ID:$RUN_GROUP_ID           \
         -v ${APP_LOG_DIR}:/log1                 \
         -v ${APP_DIR}/var/mysqld:/var/mysqld    \
         -v ${APP_DIR}/etc/mysql:/etc/mysql      \
-	    -v ${APP_DIR}/../db_data/sci:/var/lib/mysql \
+        -v ${DATA_DIR}:/var/lib/mysql           \
         -e MYSQL_ROOT_PASSWORD=$ROOT_PASSWORD   \
-        registry.aliyuncs.com/wql/mariadb:10.1
+        --oom-kill-disable                      \
+        --restart=always                        \
+        tggj/mariadb:10.1
         
-
-#    docker create --net=host \
-#        --name $DOCKER_CONTAINER_NAME			\
-#        -v ${APP_DIR}/log:/log1 			\
-#        -v ${APP_DIR}/var/mysqld:/var/mysqld		\
-#        -v ${APP_DIR}/etc/mysql:/etc/mysql		\
-#	-v ${APP_DIR}/../db_data/sci:/var/lib/mysql	\
-#        -e MYSQL_ROOT_PASSWORD=$ROOT_PASSWORD 		\
-#        registry.aliyuncs.com/wql/mariadb:10.1
-
 }
-
 
 app_client() {
-    #docker exec -it $DOCKER_CONTAINER_NAME  mysql --defaults-file=/etc/mysql/my.cnf -S /var/mysqld/mysqld.sock -u root
-    docker exec -it $DOCKER_CONTAINER_NAME  bash -c 'export TERM=xterm; mysql --defaults-file=/etc/mysql/my.cnf -S /var/mysqld/mysqld.sock -u root -p"$ROOT_PASSWORD"'
-    #docker exec -it $DOCKER_CONTAINER_NAME  bash -c 'export TERM=xterm; mysql --defaults-file=/etc/mysql/my.cnf -S /var/mysqld/mysqld.sock -u root -p'
+    docker exec -it $DOCKER_CONTAINER_NAME  bash -c 'export TERM=xterm; mysql --defaults-file=/etc/mysql/my.cnf -S /var/mysqld/mysqld.sock -u root -p'
 }
-
-
-
 
 
 ##############################################################################
