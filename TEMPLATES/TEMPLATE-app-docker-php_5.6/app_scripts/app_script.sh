18a762d8f34dcb0882a84c52b31e5f2309d7c221
wooyea <wooyea@gmail.com>
2016-05-04 18:08:52 +0800

    php-apache run failed

--- TEMPLATES/TEMPLATE-app-docker-php_5.6/app_scripts/app_script.sh
+++ TEMPLATES/TEMPLATE-app-docker-php_5.6/app_scripts/app_script.sh
@@ -30,47 +30,43 @@ source $PRG_RELATIVE_DIR/../../common/header.sh
 #        --net=host -u $RUN_USER_ID:$RUN_GROUP_ID \
 #        -v ${APP_DIR}/etc/apache2:/etc/apache2 \
 #        -v ${APP_DIR}/etc:/usr/local/etc \
-#	-v ${APP_DIR}/../websites:/var/websites \
+#        -v ${APP_DIR}/../websites:/var/websites \
 #        -v ${APP_DIR}/var/apache2:/var/apache2 \
 #        -v ${APP_DIR}/var/php_session:/var/php_session \
-#        -v ${LOG_DIR}:/log1 \
+#        -v ${APP_LOG_DIR}:/log1 \
 #        registry.aliyuncs.com/wql/php:5.6 apache2-foreground
 
 
 # apache run as root , privilege ports under 1024 needs root
 #    docker create --name $DOCKER_CONTAINER_NAME \
 #        --net=host \
-#	--cap-add net_bind_service \
+#        --cap-add net_bind_service \
 #        -v ${APP_DIR}/etc/apache2:/etc/apache2 \
 #        -v ${APP_DIR}/etc:/usr/local/etc \
-#	-v ${APP_DIR}/../websites:/var/websites \
+#        -v ${APP_DIR}/../websites:/var/websites \
 #        -v ${APP_DIR}/var/apache2:/var/apache2 \
 #        -v ${APP_DIR}/var/php_session:/var/php_session \
 #        -v ${APP_LOG_DIR}:/log1 \
 #        registry.aliyuncs.com/wql/php:5.6 apache2-foreground
-
-apache2-foreground
-
 
 init() {
 
     # run as root
     docker create --name $DOCKER_CONTAINER_NAME \
         --net=host \
-	--cap-add net_bind_service \
+        --cap-add net_bind_service \
         -v ${APP_DIR}/etc/apache2:/etc/apache2 \
         -v ${APP_DIR}/etc:/usr/local/etc \
-	-v ${APP_DIR}/../websites:/var/websites \
+        -v ${APP_DIR}/../websites:/var/websites \
         -v ${APP_DIR}/var/apache2:/var/apache2 \
         -v ${APP_DIR}/var/php_session:/var/php_session \
-        -v ${LOG_DIR}:/log1 \
+        -v ${APP_LOG_DIR}:/log1 \
         registry.aliyuncs.com/wql/php:5.6 apache2-foreground
 
 }
 
 start() {
-    su_run "mkdir asdf";
-    #docker start $DOCKER_CONTAINER_NAME
+    docker start $DOCKER_CONTAINER_NAME
 }
 
 stop() {
