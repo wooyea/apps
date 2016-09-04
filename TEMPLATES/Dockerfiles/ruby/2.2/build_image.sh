8dc5a6822f9f6239e647f69ee4b3b051c4b316ee
wooyea <wooyea@gmail.com>
2016-09-04 21:34:45 +0800

    update ruby docker template

--- TEMPLATES/Dockerfiles/ruby/2.2/build_image.sh
+++ TEMPLATES/Dockerfiles/ruby/2.2/build_image.sh
@@ -1,6 +1,12 @@
 ping mirrors.aliyuncs.com -c 2
-[ $? -eq 0 ] && cp ../../common/sources.list.jessie-aliyun sources.list.jessie \
+[ $? -eq 0 ] && cp ../../common/sources.list.jessie-aliyuncs sources.list.jessie \
     || cp ../../common/sources.list.jessie-163 sources.list.jessie
 
+cp ../../common/gosu-amd64_1.7 ./
+cp ../../common/tini_0.9.0 ./
 
 docker build -t woo/ruby:2.2 ./
+
+rm gosu-amd64_1.7
+rm tini_0.9.0
+rm sources.list.jessie
