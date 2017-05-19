0a1c00f84ec5f5770d1acf7fb63857a6eb83d7f6
liuji <liuji@scm.ipcook.net>
2017-05-19 11:42:45 +0800

    update Dockerfiles/mariadb/10.1
    Signed-off-by: liuji <liuji@scm.ipcook.net>

--- TEMPLATES/Dockerfiles/mariadb/10.1/build_image.sh
+++ TEMPLATES/Dockerfiles/mariadb/10.1/build_image.sh
@@ -1,5 +1,4 @@
 #docker pull mariadb:10.1
 docker build -t apps/mariadb:10.1 ./
 
-
-docker tag apps/mariadb:10.1 registry.aliyuncs.com/wql/mariadb:10.1
+#docker tag apps/mariadb:10.1 registry.aliyuncs.com/wql/mariadb:10.1
