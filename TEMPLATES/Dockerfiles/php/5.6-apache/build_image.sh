c9f822b8bc3d26e566d832d1583d142ef1166b71
wooyea <wooyea@gmail.com>
2019-04-24 15:31:59 +0800

    use nc instead of ping
    Signed-off-by: wooyea <wooyea@gmail.com>

--- TEMPLATES/Dockerfiles/php/5.6-apache/build_image.sh
+++ TEMPLATES/Dockerfiles/php/5.6-apache/build_image.sh
@@ -1,5 +1,5 @@
 
-ping mirrors.aliyuncs.com -c 2
+nc -z -v -w 2 mirrors.aliyuncs.com
 [ $? -eq 0 ] && cp ../../common/sources.list.stretch-aliyun sources.list \
     || cp ../../common/sources.list.stretch-163 sources.list
 
